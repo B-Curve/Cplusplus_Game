c2af47a935a7829bc2e903f5a5e5e796acb7e949
Brandon Kervin <brandonkervin@gmail.com>
2018-04-07 16:15:45 -0500

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Brandon Kervin on 4/7/18.
+//  Copyright © 2018 Brandon Kervin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
